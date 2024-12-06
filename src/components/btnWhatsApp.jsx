746a4b2dbb1b895389f107a9b835e4aebf9452d1
Kevin Andrés <andres200127@hotmail.com>
2024-12-05 23:05:34 -0500

    using btn whatsApp

--- src/components/btnWhatsApp.jsx
+++ src/components/btnWhatsApp.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { MdWhatsapp } from 'react-icons/md';
 
 export default function BtnWhatsApp(){
+    const sendMessage = () => {
+        let numero = '573174700318'
+        const mensaje = encodeURIComponent("Hola, estoy interesado/a en conocer más");
+        const enlace = `https://wa.me/${numero}?text=${mensaje}`;
+        window.open(enlace, "_blank");
+    }
     return (
         <div className="btnWhatsApp">
-            <button>
+            <button onClick={()=> sendMessage()}>
                 <MdWhatsapp className='icon' />
             </button>
         </div>
